9ee192d8e908911c55e8aa0419710baa139bf0a7
James Wainwright <james.wainwright@lowrisc.org>
2025-09-11 14:15:20 +0100

    Revert LLVM version to last released
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw-versions.sh
+++ sw-versions.sh
@@ -9,7 +9,7 @@
 # - single-byte code coverage
 export LLVM_URL=https://github.com/lowRISC/llvm-project.git
 export LLVM_BRANCH=ot-llvm-16-hardening
-export LLVM_VERSION=dec908d48facb6041c12b95b8ade64719a894917
+export LLVM_VERSION=d213f6b2e0bcc561930538ef47b3b5cc900b5b17
 
 # RISC-V fork of Binutils 2.35 with bitmanip instruction support
 export BINUTILS_URL=https://github.com/riscv-collab/riscv-binutils-gdb.git
